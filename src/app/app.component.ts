5974df415be0f3775c894476907612731591ee0c
gvasilakis <gvasilakis@iknowhow.com>
2023-09-15 16:52:52 +0300

    http communication with json-server mocking and form submit with enter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  standalone: true,
+  imports: [
+    HomeComponent,
+    RouterLink,
+    RouterOutlet,
+    NgOptimizedImage,
+  ],
+  template: `
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" ngSrc="../assets/logo.svg" height="44" width="151" alt="logo" aria-hidden="true">
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-app';
+  title = 'homes';
 }
